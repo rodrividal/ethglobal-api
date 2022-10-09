c409af20e355bf36ec5a8d43725fa92fc26c0a27
Rodrigo Vidal <rodrigo@tixcode.co>
2022-10-09 04:54:11 -0500

    Update database.js

--- services/database.js
+++ services/database.js
@@ -105,7 +105,7 @@ const watchedAdExists = async (address, message_id) => {
 }
 
 const insertWatchedAd = async (data) => {
-    if (!await watchedAdExists()) {
+    if (!await watchedAdExists(data.address, data.message_id)) {
         const { response, error } = await supabase
             .from('watched_ads')
             .insert([
